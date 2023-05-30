b700913ea0628744028158a5bc1b77d6f70dbc56
rschuquel <renan.schuquel@unimedvtrp.com.br>
2023-05-30 17:57:15 -0300

    Uncommented code

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,29 @@ use dotenv;
 fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
 
-    // let tcp = TcpStream::connect("192.168.0.121:22")?;
-    // let mut sess = Session::new()?;
+    let tcp = TcpStream::connect("192.168.0.121:22")?;
+    let mut sess = Session::new()?;
 
-    // sess.set_tcp_stream(tcp);
-    // sess.handshake()?;
+    sess.set_tcp_stream(tcp);
+    sess.handshake()?;
 
-    // match sess.userauth_password(dotenv::var("USERNAME").unwrap().as_str(), dotenv::var("PASSWORD").unwrap().as_str()) {
-    //     Ok(_) => (),
-    //     Err(error) => panic!("An error ocurred on trying to authenticate: {:?}", error)
-    // };
+    match sess.userauth_password(dotenv::var("USERNAME").unwrap().as_str(), dotenv::var("PASSWORD").unwrap().as_str()) {
+        Ok(_) => (),
+        Err(error) => panic!("An error ocurred on trying to authenticate: {:?}", error)
+    };
 
-    // let (mut remote_file, stat) = sess.scp_recv(Path::new("/var/www/html/interno/cron-job/notifications/negativas/log/negativas-2023-05-24.log"))?;
-    // println!("Remote file stats: {}", stat.size());
-    // let mut contents: Vec<u8> = Vec::new();
-    // remote_file.read_to_end(&mut contents)?;
+    let (mut remote_file, stat) = sess.scp_recv(Path::new("/var/www/html/interno/cron-job/notifications/negativas/log/negativas-2023-05-24.log"))?;
+    println!("Remote file stats: {}", stat.size());
+    let mut contents: Vec<u8> = Vec::new();
+    remote_file.read_to_end(&mut contents)?;
 
-    // let mut file = fs::File::create("/home/rschuquel/remote_log.log")?;
-    // file.write(&contents)?;
+    let mut file = fs::File::create("/home/rschuquel/remote_log.log")?;
+    file.write(&contents)?;
 
-    // remote_file.send_eof()?;
-    // remote_file.wait_eof()?;
-    // remote_file.close()?;
-    // remote_file.wait_close()?;
+    remote_file.send_eof()?;
+    remote_file.wait_eof()?;
+    remote_file.close()?;
+    remote_file.wait_close()?;
 
     let mut file = fs::File::open("/home/rschuquel/remote_log.log")?;
     let mut contents = String::new();
